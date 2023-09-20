5b72414436213842907d898e76cffb3d339111a9
JovanAleksicFutureGames <145547495+JovanAleksicFutureGames@users.noreply.github.com>
2023-09-20 13:04:52 +0200

    Updated GameManager
    Added basic functionality for keeping the score, the lost condition, the lost life logic and keeping score - TODO: Jovan - merge with the PointScoring script

--- UnityLabyrinthGame/Assets/Scripts/GameManager.cs
+++ UnityLabyrinthGame/Assets/Scripts/GameManager.cs
@@ -1,18 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public int CurrentScore { get; private set; }
+    private int _currentLives = 3;
+
+    [SerializeField] private Transform _ball;
+
+    [SerializeField] private TextMeshProUGUI _scoreText;
+    [SerializeField] private GameObject _endGameScreen;
+
     void Start()
+    {
+            
+    }
+
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void LoseState() 
     {
-        
+        if(_currentLives <= 0) 
+        {
+
+        }
+    }
+
+    private void LoseLife() 
+    {
+
+    }
+
+    public void RestartLevel() 
+    {
+        SceneManager.LoadScene("SampleScene"); 
+        //TODO: Jovan - Scene Loading could go smoother/better - need time to figure out which way I can do it. 
     }
 }
