a4a6d4871977ff7db18fd76fa47dc2b74eac20a0
Filip Clavin <filipclavin99@gmail.com>
2023-09-20 15:06:09 +0200

    Update HoleScore.cs

--- UnityLabyrinthGame/Assets/Scripts/HoleScore.cs
+++ UnityLabyrinthGame/Assets/Scripts/HoleScore.cs
@@ -7,18 +7,25 @@ public class HoleScore : MonoBehaviour
     public bool isVictoryHole = false;
 
     public int holeScore;
+    private GameManager gameManager;
+    private PointScoring pointScoring;
+
+    private void Start()
+    {
+        gameManager = FindObjectOfType<GameManager>();
+        pointScoring = FindObjectOfType<PointScoring>();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "Player" && isVictoryHole == true) 
         {
-            FindObjectOfType<GameManager>().WinState();
-
+            gameManager.WinState();
         }
         else if(other.gameObject.tag == "Player" && isVictoryHole == false) 
         {
-            FindObjectOfType<PointScoring>().score += holeScore;
-            FindObjectOfType<GameManager>().LoseState();
+            pointScoring.score += holeScore;
+            gameManager.LoseState();
 
         }
     }
