0f960cdb03edc57a98773ab86183e11c94df74ef
Filip Clavin <filipclavin99@gmail.com>
2023-09-20 13:20:53 +0200

    rotation functional

---
+++ UnityLabyrinthGame/Assets/Scripts/MazeControl.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
+
+public class MazeControl : MonoBehaviour
+{
+    [SerializeField] [Range(1f, 100f)] float sensitivity;
+    [SerializeField] InputAction mouseMovement;
+    [SerializeField] InputAction enableRotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mouseMovement.Enable();
+        enableRotation.Enable();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (enableRotation.IsPressed())
+        {
+            Vector2 mouseDelta = mouseMovement.ReadValue<Vector2>();
+            transform.rotation = Quaternion.Euler(
+                transform.eulerAngles.x + mouseDelta.y * sensitivity * Time.deltaTime,
+                0,
+                transform.eulerAngles.z + -mouseDelta.x * sensitivity * Time.deltaTime
+            );
+        }
+    }
+}
