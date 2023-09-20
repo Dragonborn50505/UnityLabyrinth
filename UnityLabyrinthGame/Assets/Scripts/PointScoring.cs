f50770e195faec2909116e59494a031b674b0c22
ElioEnestrom <elio@enestrom.com>
2023-09-20 13:20:28 +0200

    GetSetBool

--- UnityLabyrinthGame/Assets/Scripts/PointScoring.cs
+++ UnityLabyrinthGame/Assets/Scripts/PointScoring.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class PointScoring : MonoBehaviour
 {
     public int score, health;
+    bool hasScored;
     HoleScore enemyHole;
+
+    public bool GetHasScored() {  return hasScored; }
+    public void SetHasScored(bool newValue) { hasScored = newValue; }
 
     private void OnTriggerEnter(Collider other)
     {
